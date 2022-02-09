42455ceef21ebe6f842767d1e9e594074ea94925
ArekStasko <arek.stasko@gmail.com>
2022-02-09 16:56:17 +0100

    add basic view and controllers structure

--- PlantCare.Controllers/ControllersFactory.cs
+++ PlantCare.Controllers/ControllersFactory.cs
@@ -1,6 +1,6 @@
 ﻿namespace PlantCare.Controllers
 {
-    public class Class1
+    public class ControllersFactory
     {
 
     }
---
+++ PlantCare.Controllers/PlantControllers/IPlantControllers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantCare.Controllers.PlantControllers
+{
+    internal interface IPlantControllers
+    {
+    }
+}
