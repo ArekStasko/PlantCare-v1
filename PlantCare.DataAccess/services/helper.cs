e7d3242386a42778df37f9dc38b9b481a3f7292e
ArekStasko <arek.stasko@gmail.com>
2022-02-21 18:28:51 +0100

    Add helper class to prepare connection for db

---
+++ PlantCare.DataAccess/services/helper.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PlantCare.DataAccess.services
+{
+    public static class Helper
+    {
+        public static string CnnVal(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
