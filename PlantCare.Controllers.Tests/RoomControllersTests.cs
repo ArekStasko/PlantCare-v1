2b9f25f4d14157ae7ea73a8afc9e4d1b535d8dce
ArekStasko <arek.stasko@gmail.com>
2022-03-20 17:10:02 +0100

    Add tests to controllers

--- PlantCare.Controllers.Tests/RoomControllersTests.cs
+++ PlantCare.Controllers.Tests/RoomControllersTests.cs
@@ -5,6 +5,7 @@ using PlantCare.DataAccess.models;
 using PlantCare.DataAccess.services;
 using PlantCare.DataAccess;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PlantCare.Controllers.Tests
 {
@@ -56,13 +57,57 @@ namespace PlantCare.Controllers.Tests
         [Test]
         public void UpdateRoom_ShouldEdit_OneRoom()
         {
-            Assert.Pass();
+            var roomData = new List<string>()
+            {
+                "Test room name",
+                "Test room location",
+                "13",
+                "77",
+                "https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+                "03",
+                "03",
+                "2022"
+            };
+            var roomControllers = ControllersFactory.GetRoomControllers(new MockView());
+            roomControllers.CreateRoom(roomData);
+            var room = roomControllers.GetRooms().Single(room => room.RoomName == roomData[0]);
+
+            var updateData = new List<string>()
+            {
+                "Updated room name",
+                "Updated room location",
+                "14",
+                "80",
+                "https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+                "10",
+                "03",
+                "2022"
+            };
+
+            roomControllers.UpdateRoom(updateData, room.Id);
+            var roomToAssert = roomControllers.GetRoom(room.Id);
+            roomToAssert.RoomName.Should().Be(updateData[0]);
         }
 
         [Test]
         public void DeleteRoom_ShouldDelete_OneRoom()
         {
-            Assert.Pass();
+            var roomData = new List<string>()
+            {
+                "Test room name",
+                "Test room location",
+                "13",
+                "77",
+                "https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+                "03",
+                "03",
+                "2022"
+            };
+            var roomControllers = ControllersFactory.GetRoomControllers(new MockView());
+            roomControllers.CreateRoom(roomData);
+            var room = roomControllers.GetRooms().Single(room => room.RoomName == roomData[0]);
+            roomControllers.DeleteRoom(room.Id);
+            roomControllers.GetRooms().Should().NotContain(item => item.Id == room.Id);
         }
     }
 }
